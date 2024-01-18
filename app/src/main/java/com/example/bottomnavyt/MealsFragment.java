4ce562dae920834bab629c61239b2a391c16fafd
Blackout1311 <mikka.widmer@gmx.ch>
2024-01-18 16:09:51 +0100

    initial commit

---
+++ app/src/main/java/com/example/bottomnavyt/MealsFragment.java
@@ -0,0 +1,23 @@
+package com.example.bottomnavyt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MealsFragment extends Fragment {
+
+    public MealsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_meals, container, false);
+    }
+}
